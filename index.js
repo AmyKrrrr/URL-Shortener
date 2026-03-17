7ca5927df6b639ff66039aad707c7908d089fe63
Amitesh Kar <amiteshkar@gmail.com>
2026-03-17 15:26:41 +0530

    Server-Side-Rendering-EJS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const { connectToMongoDB } = require("./connect");
 
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
 
 const PORT = 8001;
@@ -12,8 +14,13 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("MongoDB Connected"),
 );
 
+app.set("view engine", "ejs"); // Mujhe ek View engine use karna hai aur mai ejs ko use karunga.
+app.set("views", path.resolve("./views"));
+
 app.use(express.json()); // parses incoming JSON data from the request body.
+app.use(express.urlencoded({ extended: false }));
 app.use("/url", urlRoute); // Whenever a request starts with /url, send it to the urlRoute router.
+app.use("/", staticRoute);
 
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
